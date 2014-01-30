457beea05827eca8a1c7c472cbe333ea2cc22a3f
Michael Ciuffo <ch00f@ch00ftech.com>
2014-01-29 23:44:12 -0800

    added stuff to read vlc table. Table is broken (maybe)

--- firmware/vlc_decoder_data.h
+++ firmware/vlc_decoder_data.h
@@ -1,11 +1,13 @@
 /**
  * @file vlc_decoder_data.h
  *
- * @author This file was procedurally generated on 26 Jan 2014
+ * @author This file was procedurally generated on 29 Jan 2014
  *
- * The decoding scheme for the Huffman encoded data used for VLC. To use, advance
- * column-wise through the matrix for 0 bits and row-wise for 1 bits. When a valid
- * symbol is encountered, record that value and resume decoding from the start.
+ * The decoding scheme for the Huffman encoded data used for VLC. The value at
+ * an index represents either the index in the array of its left child or, if
+ * the most significant bit is set, the value itself. The index of the right
+ * child is the stored index value + 1. If there is no right or left child, the
+ * value at that index will equal VLC_DECODER_DATA_INVALID
  */
 
 #ifndef VLC_DECODER_DATA_H_
@@ -14,14 +16,13 @@
 #include <stdint.h>
 #include <avr/pgmspace.h>
 
-/// The number of elements in the matrix
-#define VLC_DECODER_DATA_ELEMENTS (100)
+enum {
+    /// The number of elements in the matrix
+    VLC_DECODER_DATA_ELEMENTS = 72,
 
-/// The number of columns in each row of the matrix
-#define VLC_DECODER_DATA_COLUMNS  (10)
-
-/// The value used to denote invalid symbols
-#define VLC_DECODER_DATA_INVALID  (0xff)
+    /// The value used to denote invalid symbols
+    VLC_DECODER_DATA_INVALID  = 0xff,
+};
 
 /// The decoder matrix
 extern const uint8_t VLC_DECODER_DATA[VLC_DECODER_DATA_ELEMENTS] PROGMEM;
