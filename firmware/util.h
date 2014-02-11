2a1dc35616725991b831f79061b35ec8b6a78c51
Jason Haensly <jason.haensly@gmail.com>
2014-02-10 19:12:40 -0800

    Fix OUTPUT_VALUE compiler warning
    The issue involved the C language integer promotion rules.

--- firmware/util.h
+++ firmware/util.h
@@ -15,7 +15,7 @@
 
 #define FIND_MIDPOINT(min, max) ((min) + (((max) - (min)) / 2))
 
-#define OUTPUT_VALUE(v) do { PORTB = ~((uint8_t)(v << 6) | (uint8_t)(v >> 2)); } while(0)
+#define OUTPUT_VALUE(v) do { PORTB = (uint8_t)~((v << 6) | (v >> 2)); } while(0)
 
 #define SIZEOF_ARRAY(a) (sizeof(a) / sizeof(a[0]))
 
